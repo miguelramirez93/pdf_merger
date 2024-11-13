af9bf52c25d46313dcdeedd09f26a684b49e60ac
Jose Miguel Ramirez <miguelramirez93@Joses-MacBook-Pro.local>
2024-11-12 22:36:20 -0500

    Add same file use case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import PyPDF2
 import os
+import re
 
 merger = PyPDF2.PdfMerger()
 
@@ -13,12 +14,27 @@ def merge_pdf_files_recursive(path="."):
             merge_pdf_files_recursive(full_path)
         else:
             if full_path.endswith(".pdf"):
-                if mergers_indx.get(path) is None:
-                    mergers_indx[path] = PyPDF2.PdfMerger()
-                try:
-                    mergers_indx[path].append(full_path)
-                except Exception as e:
-                    print(f"file path: {full_path} error:{str(e)}")
+                add_file_to_merge_indx(path, full_path)
+
+
+def merge_pdf_files_samename_recursive(path="."):
+    for entry in os.listdir(path):
+        full_path = os.path.join(path, entry)
+        if os.path.isdir(full_path):
+            merge_pdf_files_samename_recursive(full_path)
+        else:
+            if full_path.endswith(".pdf"):
+                index = get_file_index(full_path)
+                add_file_to_merge_indx(index, full_path)
+
+
+def add_file_to_merge_indx(index, full_path):
+    if mergers_indx.get(index) is None:
+        mergers_indx[index] = PyPDF2.PdfMerger()
+    try:
+        mergers_indx[index].append(full_path)
+    except Exception as e:
+        print(f"file path: {full_path} error:{str(e)}")
 
 
 def get_file_name_from_path(path="combined") -> str:
@@ -26,10 +42,17 @@ def get_file_name_from_path(path="combined") -> str:
     return splitted[-1]
 
 
+def get_file_index(path="combined") -> str:
+    splitted = path.split("/")
+    rawFileName = splitted[-1]
+    return re.sub(r"\([0-9]+\).pdf", "", rawFileName).replace(".pdf", "")
+
+
 directory_path = "./target"
 outdir_path = "./out"
 try:
     merge_pdf_files_recursive(directory_path)
+    # merge_pdf_files_samename_recursive(directory_path)
     for dir in mergers_indx:
         mergers_indx[dir].write(f"{outdir_path}/{get_file_name_from_path(dir)}.pdf")
 except Exception as e:
